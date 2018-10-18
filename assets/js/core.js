ee60a91797cf15574bb267de05fb634a057da43c
Grafffffff <Antonok94@gmail.com>
2018-10-18 18:36:03 +0400

    add catagory and counter class js

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,72 +1,115 @@
-var filter = [];
-var category_id;
-var url = document.location.origin + '/filters';
-$('#submit_filters').on('click', function () {
-    submitFilters();
-});
+"use strict";
+//Функция аккордеона. Переключает класс .active между элементами, на активный
+function accordeonClass(collection, item) {
+    collection.forEach(function(item){
+        item.classList.remove('active');
+    })
+    item.classList.add('active');
+}
+//Счетчик, на вход которого можно подавать стартовое значение
+//По умолчанию стартовое значение равно нулю
+function Counter(number) {
+    let count = number || 0;
+    
+    this.plus = () => count++;
+    
+    this.minus = () => count--;
+    
+    this.getCount = () => count;
+    
+    this.setCount = (index) => count = index; 
+}
 
-function submitFilters() {
-    var countFilter = $('.product-form__body .product-form__item').length;
-    for (var i = 0; i < countFilter; i++) {
-        id = $('.product-form__item').eq(i).find('.filter_name').attr('filter_id');
-        countSelectFilter = $('.product-form__item').eq(i).find('.lighted').length;
-        parentFilter = $('.m-catalog__left-check-block').eq(i).children('.lighted');
-        option = $('.product-form__item').eq(i).find('.product-form__select').val();
-        if (option != 'all') {
-            filter.push({
-                id,
-                option
-            });
+//Класс Слайдер, на вход которого подаются:
+//Коллекция слайдов, стартовое значение и таймер.
+//Автопереключение В СЕКУНДАХ(если не нужен, просто подать 0)!!!
+function Slider(items, number = 0, timer) {
+    Counter.apply(this);
+    console.log('Autorun '+timer+'s');
+
+    const that = this;
+    
+    this.setCount(number);
+    
+    this.check = function () {
+        if (that.getCount() >= items.length) {
+            that.setCount(0);
+        } 
+        if (that.getCount() < 0) {
+            that.setCount(items.length - 1);
         }
     }
-    category_id = $('.category_id').attr('category_id');
-    page = $('.category_id').attr('page');
-    sendDataPartial(filter, category_id, page);
-}
-
-function sendDataPartial(f, id, page) {
-    if (f.length == 0) {
-        f = 101;
+    
+    this.next = function() {
+        that.plus();
+        that.check();
+        
     }
-    $.ajax({
-        method: 'POST',
-        url: url,
-        data: {
-            "data": f,
-            "id": id,
-            "page": page
-        },
-        cache: false,
-        beforeSend: function () {
-            preloaderShow();
-        },
-        success: function (x) {
-            preloaderHide();
-            $('.paste-ajax').html(x);
+    
+    this.prev = function(){
+        that.minus();
+        that.check();
+    }
+    
+//    if (timer) {
+//        console.log(timer);
+//        setInterval(this.next(), timer*1000);
+//        console.log(timer);
+//    }
+}
+
+
+//JS для главной страницы
+document.addEventListener('DOMContentLoaded', function () {
+    "use strict";
+    //Блок "Спецпредложение"
+    let specialSlides = document.querySelectorAll('.special-slider__content-column');
+    let specialSliderPrev = document.querySelector('.special-slider__arrows-left');
+    let specialSliderNext = document.querySelector('.special-slider__arrows-right');
+    
+    function SpecialSlider(items, number = 0, timer, btnPrev, btnNext) {
+        Slider.apply(this, arguments);
+        
+        const that = this;
+        
+        this.display = function() {
+            accordeonClass(items, items[that.getCount()], items[that.getCount()+1]);
+            if(window.matchMedia('(min-width: 768px)').matches) {
+                if (that.getCount() >= items.length-1) {
+                    items[0].classList.add('active');
+                } else {
+                    items[that.getCount()+1].classList.add('active');
+                }
+            }
         }
-    });
-}
-
-function preloaderShow() {
-    var selector = '#preloader';
-    var si = 700;
-    $(selector).attr("complite", "false");
-    setTimeout(function () {
-        if ($(selector).attr("complite") === "false") {
-            $(selector).fadeIn(300);
-            $('body').css('cursor', 'wait');
+        
+        btnPrev.onclick = function() {
+            that.prev();
+            that.display();
         }
-    }, si);
-}
-
-function preloaderHide() {
-    var selector = '#preloader';
-    $(selector).attr("complite", "true");
-    $(selector).hide();
-    $('body').css('cursor', 'default');
-}
-
-function sendPage(page) {
-    $('.category_id').attr('page', page);
-    $('#submit_filters').click();
-}
+        btnNext.onclick = function() {
+            that.next();
+            that.display();
+        }
+    }
+    
+    const specialSlider = new SpecialSlider(
+        specialSlides, 
+        0,
+        2,
+        specialSliderPrev, 
+        specialSliderNext
+    );
+    
+    //Блог категорий товарос с описанием для сео
+    let categoryesBtns = document.querySelectorAll('.categoryes-menu__item');
+    let categoryesText = document.querySelectorAll('.categoryes-desc');
+    
+    for (let i = 0; i < categoryesBtns.length; i++ ) {
+        categoryesBtns[i].id = i;
+        categoryesBtns[i].onclick = function() {
+            accordeonClass(categoryesBtns, this);
+            accordeonClass(categoryesText, categoryesText[this.id]);
+        }
+    }
+})
