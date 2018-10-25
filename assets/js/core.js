ed481390319e41ee4b6c2199b86966f97e1bdaf7
Grafffffff <Antonok94@gmail.com>
2018-10-25 16:26:26 +0400

    catalog complete
    add slider in categoryes of catalog

--- assets/js/core.js
+++ assets/js/core.js
@@ -11,13 +11,13 @@ function accordeonClass(collection, item) {
 //По умолчанию стартовое значение равно нулю
 function Counter(number) {
     let count = number || 0;
-    
+
     this.plus = () => count++;
-    
+
     this.minus = () => count--;
-    
+
     this.getCount = () => count;
-    
+
     this.setCount = (index) => count = index; 
 }
 
@@ -27,11 +27,11 @@ function Slider(quantity, number = 0) {
     Counter.apply(this);
 
     const that = this;
-    
+
     this.setCount(number);
-    
+
     this.show = () => that.getCount();
-    
+
     this.check = function () {
         if (that.getCount() >= quantity) {
             that.setCount(0);
@@ -41,17 +41,17 @@ function Slider(quantity, number = 0) {
         }
         that.show();
     }
-    
+
     this.next = function() {
         that.plus();
         that.check();
     }
-    
+
     this.prev = function(){
         that.minus();
         that.check();
     }
-    
+
     this.timer = (time) => setInterval(() => that.next(), time*1000);
 }
 
@@ -88,45 +88,44 @@ function EasySlider(items, number = 0, time, btnPrev, btnNext) {
 function EasyCounter(number, display, btnPlus, btnMinus) {
     Counter.apply(this, [number]);
     const that = this;
-    
+
     btnPlus.onclick = function() {
         that.plus();
         that.display();
     }
-    
+
     btnMinus.onclick = function() {
         that.minus();
         that.display();
     }
-    
+
     this.display = function() {
         (that.getCount() >= 0) ? display.value = that.getCount() : display.value = '0';
         display.onchange();
     }
 }
 
-
 //Для всех страниц
 document.addEventListener('DOMContentLoaded', function () {
     //Обработка событий на шапке
     //Кнопка "Каталог"
     let headerCatalogBtn = document.querySelector('.header__catalog');
-    
+
     headerCatalogBtn.onclick = function() {
         this.classList.toggle('active');
     }
     //Сэндвич меню в шапке мобильной версии
     let sandwich = document.querySelector('.header__sandwich');
     let headerNav = document.querySelector('.header__nav');
-    
+
     sandwich.onclick = () => headerNav.classList.toggle('d-none');
-    
+
     //Поисковая строка в мобильной версии
     let search = document.querySelector('.search');
     let searchGo = document.querySelector('.search-panel__go');
     let searchClose = document.querySelector('.search-panel__close');
     let searchActive = false;
-    
+
     searchGo.onclick = function () {
         if (searchActive) {
             return console.log('searching...');
@@ -135,22 +134,22 @@ document.addEventListener('DOMContentLoaded', function () {
             return searchActive = true;
         }
     }
-    
+
     searchClose.onclick = function () {
         search.classList.remove('active');
         return searchActive = false;
     }
-    
-    
-    
+
+
+
     //Счетчики: определяем все счетчики на странице.
     let counterTablets = document.querySelectorAll('.counter-tablet');
-    
+
     for (let i = 0; i < counterTablets.length; i++ ) {
         let counterPlus = counterTablets[i].nextElementSibling;
         let counterMinus = counterTablets[i].previousElementSibling;
         counterTablets[i].onchange = function() {return null}
-        
+
         const counter = new EasyCounter(counterTablets[i].value, counterTablets[i], counterPlus, counterMinus);
     }
 });
@@ -160,7 +159,7 @@ document.addEventListener('DOMContentLoaded', function () {
     "use strict";
     let catalogMenuItems = document.querySelectorAll('.catalog-menu__item');
     let catalogSubmenuItems = document.querySelectorAll('.catalog-menu__submenu-item');
-        
+
     for (let i = 0; i < catalogMenuItems.length; i++ ) {
         catalogMenuItems[i].id = i;
         catalogMenuItems[i].onclick = function() {
@@ -170,7 +169,7 @@ document.addEventListener('DOMContentLoaded', function () {
             accordeonClass(catalogMenuItems, this);
         }
     }
-    
+
     for (let i = 0; i < catalogSubmenuItems.length; i++ ) {
         catalogSubmenuItems[i].id = i;
         catalogSubmenuItems[i].onclick = function(ev) {
@@ -178,6 +177,47 @@ document.addEventListener('DOMContentLoaded', function () {
             ev.stopPropagation();
         }
     }
+
+    //Слайдер с товарами
+    let catalogCategoryes = document.querySelectorAll('.catalog-content__category');
+
+    //Класс для работы каталоговых слайдеров, слайды двигаются на определенный % 
+    function CatalogSlider(obj) {
+        let position = +obj.slider.style.left.replace(/\D/g, '') || 0;
+        Slider.apply(this, [obj.slider.children.length-2, position/100]);
+
+        this.display = () => {
+            if(window.matchMedia('(min-width: 768px)').matches) {
+                return obj.slider.style.left = -this.show()*100/3 + '%';
+            }
+            return obj.slider.style.left = -this.show()*100/2 + '%';
+        }
+
+        obj.next.onclick = () => {
+            this.next();
+            return this.display();
+        }
+
+        obj.prev.onclick = () => {
+            this.prev();
+            return this.display();
+        }
+    }
+
+    catalogCategoryes.forEach(function(category) {
+        const catalogCategory = {
+            conteiner: category,
+            prev: category.querySelector('.arrow-left'),
+            next: category.querySelector('.arrow-right'),
+            slider: category.querySelector('.catalog-content__category-wrapper'),
+        };
+        if (catalogCategory.slider) {
+            const catalogSlider = new CatalogSlider(catalogCategory);
+        }
+    })
+
+
+
 });
 
 //JS for productpage
@@ -190,24 +230,24 @@ document.addEventListener('DOMContentLoaded', function () {
     let galleryBtnClose = document.querySelector('.productpage-gallery__btn-close');
     let galleryBtnLeft = document.querySelector('.productpage-gallery__arrows-left');
     let galleryBtnRight = document.querySelector('.productpage-gallery__arrows-right');
-    
+
     galleryBtnOpen.onclick = () => galleryModal.style.display = 'flex';
     galleryModal.onclick = () => galleryModal.style.display = 'none';
     galleryBtnClose.onclick = () => galleryModal.style.display = 'none';
     galleryWrapper.onclick = (ev) => ev.stopPropagation();
-    
+
     const productSlider = new EasySlider(galleryItems, 0, 0, galleryBtnLeft, galleryBtnRight);
-    
+
     //Кнопочка купить
-    
+
     let galleryBtnBuy = document.querySelector('.productpage-panel__props-btn');
-    
+
     galleryBtnBuy.onclick =() => alert('Добавлено в корзину!');
-    
+
     //Описание товара
     let descBtns = document.querySelectorAll('.productpage-desc__points-item');
     let descText = document.querySelectorAll('.productpage-desc__text-item');
-    
+
     for (let i = 0; i < descBtns.length; i++ ) {
         descBtns[i].id = i;
         descBtns[i].onclick = function() {
@@ -220,34 +260,34 @@ document.addEventListener('DOMContentLoaded', function () {
 //JS для главной страницы
 document.addEventListener('DOMContentLoaded', function () {
     "use strict";
-    
+
     //Главный слайдер на главной странице
     let sliderSlides = document.querySelectorAll('.slider-content__item');
     let sliderSliderPrev = document.querySelector('.slider__arrow-left');
     let sliderSliderNext = document.querySelector('.slider__arrow-right');
-    
+
     const sliderSlider = new EasySlider(sliderSlides, 2, 3, sliderSliderPrev, sliderSliderNext);
-    
+
     sliderSlider.display();
-    
+
     //Блок "Спецпредложение"
     let specialSlides = document.querySelectorAll('.special-slider__content-column');
     let specialSliderPrev = document.querySelector('.special-slider__arrows-left');
     let specialSliderNext = document.querySelector('.special-slider__arrows-right');
-    
+
     //Добавляем первый элемент в конец родителя, без внесения в келлекцию
     //Делается для того что бы последним элементом показывался первый без разрыва
     let clone = specialSlides[0].cloneNode(true);
     specialSlides[0].parentNode.appendChild(clone);
-    
+
     const specialSlider = new EasySlider(specialSlides, 2, 6, specialSliderPrev, specialSliderNext);
-    
+
     specialSlider.display();
-    
+
     //Блог категорий товарос с описанием для сео
     let categoryesBtns = document.querySelectorAll('.categoryes-menu__item');
     let categoryesText = document.querySelectorAll('.categoryes-desc');
-    
+
     for (let i = 0; i < categoryesBtns.length; i++ ) {
         categoryesBtns[i].id = i;
         categoryesBtns[i].onclick = function() {
@@ -258,24 +298,24 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-//НЕОБХОДИМЫ СЕРЕЗНЫЕ РАБОТЫ ПО СОЗДАНИЮ ОБЪЕКТА КОРЗИНЫ
+//НЕОБХОДИМЫ СЕРЬЕЗНЫЕ РАБОТЫ ПО СОЗДАНИЮ ОБЪЕКТА КОРЗИНЫ
 //JS for basket
 document.addEventListener('DOMContentLoaded', function () {
     let counterTablets = document.querySelectorAll('.counter-tablet');
     let totalCost = document.querySelector('.basket-bottom__info-cost .num');
     let localCost = document.querySelectorAll('.basket-table__item-cost .num');
-    
+
     function totalSum() {
         totalCost.innerText = '0';
         for (let i = 0; i < localCost.length; i++ ) {
             totalCost.innerText = +totalCost.innerText + +localCost[i].innerText;
         }
     }
-    
+
     counterTablets.forEach(function(item) {
         let priceNum = item.parentNode.parentNode.querySelector('.basket-table__item-price .num');
         let costNum = item.parentNode.parentNode.querySelector('.basket-table__item-cost .num');
-        
+
         if (costNum) {
             item.onchange = function() {
                 let quantity = +item.value;
@@ -284,12 +324,12 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     });
-    
+
     totalSum();
-    
+
     let basketDropBtns = document.querySelectorAll('.basket-table__item-cansel');
     let quantityOfProducts = basketDropBtns.length;
-    
+
     basketDropBtns.forEach(function(item){
         item.onclick = function(){
             this.parentNode.parentNode.remove();
@@ -297,17 +337,17 @@ document.addEventListener('DOMContentLoaded', function () {
             totalSum();
         }
     });
-    
+
     let basketProgressFields = document.querySelectorAll('.basket-progress__box-item');
     let basketContainers = document.querySelectorAll('.basket-container');
     let basketBtnNext = document.querySelector('.basket-bottom__btn-next');
-    
+
     basketBtnNext.number = 1;
-    
+
     basketBtnNext.onclick = function() {
         accordeonClass(basketProgressFields, basketProgressFields[basketBtnNext.number]);
         accordeonClass(basketContainers, basketContainers[basketBtnNext.number]);
         basketBtnNext.number++;
     }
-    
+
 });
