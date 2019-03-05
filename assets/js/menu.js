ceddf52efbda2b2f27ef901d86d365f7f9f14e6f
Grafffffff <Antonok94@gmail.com>
2019-03-06 00:53:03 +0400

    add es6 class to catalog

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -23,107 +23,40 @@ const catalogLayout = {
     }
 }
 
-class List {
-    constructor(propList) {
-        this.list = {
-            id: propList.id || '',
-            tag: propList.tag || 'ul',
-            title: propList.title || '',
-            className: propList.className || '',
-            elements: propList.elements || [],
-        }
+const normalizeData = data => {
+    let parsed = {
+        heads: {},
+        subs: {},
+        links: {}
     }
-    createList = () => {
-        const currentList = createElement({
-            ...this.list,
-            elements: null
-        })
-        this.list.elements.forEach(item => {
-            
-            console.log('currentList', currentList)
-            currentList.appendChild(createElement({
-                ...item,
-                elements: null
-            }))
-        })
-        return currentList
-    }
-}
 
-fetch("catalog.json")
-    .then(res => res.json())
-    .then(response => createCatalog(initCatalog))
-    .catch(error => createCatalog(initCatalog))
+    data.forEach(item => parsed.heads[item.id] = {...item, parent: 'catalog'})
 
-function createCatalog(catalog) {
-    if (!catalog && !catalog.length) return createCatalog(initCatalog);
-    
-    let dropdown = document.querySelector('.header__catalog');
-    
-    const menu = new List({
-        title: 'Каталог',
-        ...catalogLayout.menu,
-        elements: catalog.map(item => {
-            return {...item,
-                ...catalogLayout.menuItem,
-                elements: [] 
-           }
+    function letsParse(from, to){
+        Object.keys(parsed[from]).forEach(headId => {
+        return parsed[from][headId].subgroup 
+            ? (
+                parsed[from][headId].subgroup.forEach(item => parsed[to][item.id] = {...item, parent: headId}),
+                parsed[from][headId].subgroup = parsed[from][headId].subgroup.map(item => item.id)
+            )
+            : null 
         })
-    })   
-    
-    if (window.matchMedia("(min-width: 992px)").matches) {
-        dropdown.parentNode.appendChild(createList({
-            ...catalogLayout.menu,
-            elements: catalog.map(item => {
-                return {...item,
-                ...catalogLayout.menuItem,
-                elements: item.subgroup 
-                    ? {
-                        ...catalogLayout.submenu,
-                        elements: item.subgroup.map(item => {
-                            return {
-                                ...item,
-                                ...catalogLayout.list,
-                                elements: item.subgroup
-                                    ? item.subgroup.map(item => {
-                                            return {
-                                                ...item,
-                                                ...catalogLayout.link
-                                            }
-                                        })
-                                    : null
-                            }
-                        })
-                    }
-                    : null
-            }})
-        }))
-    } else {
-        console.log('small', window.visualViewport.width, catalog)
     }
-}
-            
-function createList(list, parent) {
-    console.log('list', list)
-    const currentList = createElement({
-        ...list,
-        elements: null
-    })
-    list.elements.forEach(item => {
-        currentList.appendChild(createElement(item))
-    })
-    return currentList
+
+    letsParse('heads', 'subs')
+    letsParse('subs', 'links')
+
+    return parsed
 }
 
 function createElement(inputElement) {
-    console.log('create element', inputElement)
     const element = {
         id: inputElement.id || '',
         tag: inputElement.tag || 'div',
-        className: inputElement.className || '',
+        className: inputElement.className || 'empty',
         title: inputElement.title || '',
         link: inputElement.link || '',
-        innerElements: inputElement.elements || null,
+        childEl: inputElement.childEl || null,
         onClick: inputElement.click || null
     }
     
@@ -140,27 +73,115 @@ function createElement(inputElement) {
     
     outputEl.appendChild(title)
     
-    if (element.innerElements && !element.innerElements.length) {
-        outputEl.appendChild(createList(element.innerElements))
-    } 
-    if (element.innerElements && element.innerElements.length) {
-        console.log('create inner element')
-        element.innerElements.forEach(item => {
-            outputEl.appendChild(createElement(item))
-        })
-    }
+    if (element.childEl) outputEl.appendChild(element.childEl)
     
     return outputEl
 }
 
-
-
-
-
-class DesktopMenu extends List {
-    constructor() {
-        super()
-        
-        
+class List {
+    constructor(propList) {
+        this.list = {
+            id: propList.id || '',
+            tag: propList.tag || 'ul',
+            title: propList.title || '',
+            className: propList.className || '',
+            elements: propList.elements || [],
+        }
+    }
+    createList = () => {
+        const currentList = createElement({
+            ...this.list
+        })
+        this.list.elements.forEach(item => {
+            currentList.appendChild(createElement({...item}))
+        })
+        return currentList
     }
 }
+
+class SubCatalog {
+    constructor(lvl2, lvl3) {
+        this.lvl2 = lvl2
+        this.lvl3 = lvl3
+    }
+    
+    get createGroups() {
+        return new List({
+            ...catalogLayout.submenu,
+            elements: Object.values(this.lvl2).map(item => (
+                {
+                    ...catalogLayout.list,
+                    ...item,
+                    childEl: item.subgroup && this.lvl3 ? new List({elements: this.getMap(item.subgroup, this.lvl3)}).createList() : null
+                }
+            ))
+        }).createList()
+    }
+    
+    getMap = (ids, data) => {
+        return ids.map(id => ({...catalogLayout.link, ...data[id]}))
+    }
+}
+
+class Catalog {
+    constructor(lvl1, lvl2 = null, lvl3 = null) {
+        this.list = lvl1
+        this.lvl2 = lvl2
+        this.lvl3 = lvl3
+    }
+    
+    get createLvl() {
+        return new List({
+            ...catalogLayout.menu,
+            elements: Object.values(this.list).map(item => (
+                {
+                    ...catalogLayout.menuItem,
+                    ...item,
+                    childEl: item.subgroup && this.lvl2 
+                        ? new SubCatalog(this.getObject(item.subgroup, this.lvl2), this.lvl3).createGroups 
+                        : null
+                }
+            ))
+        }).createList()
+    }
+    
+    getObject = (ids, data) => {
+        return ids.reduce((acc, item) => {
+            return {
+                ...acc,
+                [item]: data[item]
+            }
+        }, {})
+    }
+}
+
+let menuData = '';
+
+fetch("catalog.json")
+    .then(res => res.json())
+    .then(response => menuData = normalizeData(response))
+    .then(() => createCatalog(menuData))
+    .catch(error => {
+        menuData = normalizeData(initCatalog)
+        return createCatalog(menuData)
+    })
+
+function createCatalog(catalog) {
+    if (!catalog) return setTimeout(() => {createCatalog(menuData)}, 1000);
+    
+    let dropdown = document.querySelector('.header__catalog');
+
+    let DesktopCatalog = new Catalog(catalog.heads, catalog.subs, catalog.links)
+    let MobileCatalog = new Catalog(catalog.heads)
+    
+    console.log(DesktopCatalog.createLvl)
+    
+    
+    if (window.matchMedia("(min-width: 992px)").matches) {
+        dropdown.parentNode.appendChild(DesktopCatalog.createLvl)
+    } else {
+        dropdown.parentNode.appendChild(MobileCatalog.createLvl)
+    }
+}
+            
+window.onresize = () => createCatalog(menuData);
